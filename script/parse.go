a3df8acdd011bb0a64663e93715c4bcbdd35cda1
Tom <tom@contextfree.info>
2025-11-18 05:23:59 -0700

    Include missed parsing code

--- script/parse.go
+++ script/parse.go
@@ -9,9 +9,9 @@ func Parse(tokens []Token) ParseNode {
 }
 
 type ParseNode struct {
-	kind  ParseKind
-	kids  []ParseNode
-	token Token
+	Kind  ParseKind
+	Kids  []ParseNode
+	Token Token
 }
 
 type ParseKind int
@@ -21,6 +21,7 @@ const (
 	ParseBlock
 	ParseCall
 	ParseFun
+	ParseJunk
 	ParseModify
 	ParseParams
 	ParseString
@@ -32,4 +33,41 @@ type parser struct {
 	tokens []Token
 	nodes  []ParseNode
 	work   []ParseNode
+}
+
+func (p *parser) parse() {
+	p.parseBlock()
+}
+
+func (p *parser) has() bool {
+	return p.index < len(p.tokens)
+}
+
+func (p *parser) peek() Token {
+	return p.tokens[p.index]
+}
+
+func (p *parser) push(node ParseNode) {
+	p.work = append(p.work, node)
+}
+
+func (p *parser) parseBlock() {
+	for p.has() {
+		p.parseStatement()
+	}
+}
+
+func (p *parser) parseStatement() {
+	start := len(p.work)
+Statement:
+	for p.has() {
+		t := p.peek()
+		p.push(ParseNode{Kind: ParseToken, Token: t})
+		p.index++
+		if t.Kind == TokenVSpace {
+			break Statement
+		}
+	}
+	_ = start
+	// TODO p.commit(ParseJunk, start)
 }
