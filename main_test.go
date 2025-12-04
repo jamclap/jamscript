febb9fc52c05686ba303b2fe65f4be9038ac77db
Tom <tom@contextfree.info>
2025-12-04 08:24:43 -0700

    Focus bench on reuse

--- main_test.go
+++ main_test.go
@@ -11,28 +11,13 @@ func BenchmarkProcessHi(b *testing.B) {
 	process(hi, b)
 }
 
-func BenchmarkResetHi(b *testing.B) {
-	processReset(hi, b)
-}
-
 func BenchmarkProcessExplore(b *testing.B) {
 	process(explore, b)
-}
-
-func BenchmarkResetExplore(b *testing.B) {
-	processReset(explore, b)
 }
 
 func process(source string, b *testing.B) {
 	e := script.NewEngine()
 	for b.Loop() {
-		e.Process(source)
-	}
-}
-
-func processReset(source string, b *testing.B) {
-	for b.Loop() {
-		e := script.NewEngine()
 		e.Process(source)
 	}
 }
